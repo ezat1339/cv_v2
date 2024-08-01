e4beb1f4dd392b965a3b61fffd89f146ab9d139b
hassan <ezath1227@gmail.com>
2024-08-01 14:30:08 +0300

    this is all file for cv virsion 2

---
+++ main_٠٧٢٧٠٤.js
@@ -0,0 +1,9 @@
+function showsidebar() {
+    var side = document.querySelector('.sidebar');
+    side.style.display='flex';
+   }
+   function hidesidebar() {
+    var side = document.querySelector('.sidebar');
+    side.style.display = 'none';
+   }
+   
